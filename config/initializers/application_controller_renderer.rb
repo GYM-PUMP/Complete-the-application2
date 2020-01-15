50a86bc21eda37a40f994f29969d535c05996c20
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-01-15 09:33:25 +0000

    [start&finish] ２章/３章/４章

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '012153d48c3818c635114363ead5eb9d4b640fbd43cacbafc9c7ca6cbcd31a6965669b84aed1864c7ae697b7fabb883e2365e93b056a88e3f537f7f954a1a96c'
